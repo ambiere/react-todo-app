7da7bf1d1e94593f9ccc653cb8f6a422fd2d6f5f
zhid0399123 <zhid0399123@gmail.com>
2023-08-14 21:49:19 +0300

    feat(dropdown): added a dropdown component

---
+++ frontend/src/components/input/dropdown/Dropdown.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useRef, useState } from "react";
+import "./dropdown.css";
+
+type DropdownPropsType = {
+  options: string[];
+  className: string;
+  placeholder: string;
+};
+
+type CurrentTarget = {
+  currentTarget: EventTarget & HTMLDivElement;
+};
+
+function Dropdown({ options, className, placeholder }: DropdownPropsType) {
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const [selected, setSelected] = useState("");
+  const [indexSelected, setIndexSelected] = useState<number>();
+
+  const handleSelection = (option: string) => {
+    setSelected(option);
+    setIndexSelected(options.indexOf(option));
+  };
+
+  const openDropdown = ({ currentTarget }: CurrentTarget) => {
+    currentTarget.nextElementSibling?.classList.toggle("dropdown-input-wrapper--focused");
+  };
+
+  useEffect(() => {
+    document.addEventListener(
+      "click",
+      () => {
+        dropdownRef.current?.classList.contains("dropdown-input-wrapper--focused") &&
+          dropdownRef.current?.classList.remove("dropdown-input-wrapper--focused");
+      },
+      true,
+    );
+  }, []);
+
+  return (
+    <div className={`dropdown ${className}`}>
+      <div className="dropdown-input-wrapper" onClick={openDropdown}>
+        <input
+          className="dropdown-input"
+          type="text"
+          name=""
+          id=""
+          value={selected}
+          placeholder={placeholder}
+          readOnly
+        />
+        <span className="dropdown-icon"></span>
+      </div>
+      <div className="dropdown-options-wrapper" ref={dropdownRef}>
+        {options.map((option, index) => (
+          <div
+            key={crypto.randomUUID()}
+            data-value={option}
+            data-index={index}
+            className={`dropdown-option ${indexSelected === index ? "selected-option" : ""}`}
+            onClick={() => handleSelection(option)}
+          >
+            <span>{option}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
